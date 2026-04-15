421a61cc3b1a1a1f9dd873fbf2815119474873ec
Ivan Gonzalez Suarez <ivan_gs_20@hotmail.com>
2026-04-15 19:27:41 +0200

    chore: remove node_modules directory and project configuration files

--- agente-impresora-v2/iniciar-agente.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Manuela Desayuna - Agente de Impresora
-cd /d "%~dp0"
-npx electron .
-pause
--- agente-impresora/iniciar-agente.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ==========================================
-echo MANUELA DESAYUNA - INICIANDO AGENTE DE IMPRESION
-echo ==========================================
-echo.
-cd /d "%~dp0"
-node index.js
-pause
